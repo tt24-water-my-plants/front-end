b909c83627810fc2f54b62bb3128ded371d7dbf5
ndossett <niki.m.dossett@gmail.com>
2020-11-16 17:45:09 -0500

    set up login page. still need to clear form

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
-function Login() {
+const initialLoginValues = {username: '', password: ''}
+
+export default function Login() {
+    const [users, setUsers] = useState([])
+    const [loginValues, setLoginValues] = useState(initialLoginValues)
+
+    const change = (evt) => {
+      const { name, value } = evt.target
+      setLoginValues({...loginValues, [name] : value})
+      console.log(initialLoginValues)
+    }
+
+    
+      const newUser = { 
+        username: loginValues.username.trim(),
+        password: loginValues.password.trim(),
+    }
+
+
     return (
-        <div>
-          Login Page  
+      <>
+        <div className='login-container'>
+          <h1>Welcome Back</h1>
         </div>
-    )
+        <form>
+          <input
+            name='username'
+            type='text'
+            value={loginValues.username}
+            onChange={change}
+          />
+          <input
+            name='password'
+            type='text'
+            value={loginValues.password}
+            onChange={change}
+          />
+        </form>
+        <h5>Forgot Password</h5>
+        <button>Sign In</button>
+      </>
+   )
 }
-
-export default Login
