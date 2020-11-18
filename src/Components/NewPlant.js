c9c5bc9a18d9b8c15b62f9946b0f6fdc3fc5fc00
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-18 15:41:45 -0500

    minor tweaks.  need functionality and values to change on type

--- src/Components/NewPlant.js
+++ src/Components/NewPlant.js
@@ -1,14 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
+
 import './componentStyles/NewPlant.css';
 
+const initialPlant = {
+  id: null,
+  species: '',
+  h2o_frequency: '',
+  img_url: ''
+}
 
 
-
-const NewPlant = () => {
+const NewPlant = (props) => {
+  const [ formVal, setFormVal ] = useState(initialPlant);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    const data = formVal;
+    console.log(data);
     console.log('click!');
+  }
+
+  const handleChanges = (e) => {
+    e.persist();
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormVal({
+      ...formVal,
+      [name]: value,
+    });
+    console.log('new values: ', formVal)
   }
 
 
@@ -18,26 +39,32 @@ const NewPlant = () => {
       <label>
           <input 
             type='text'
-            name='name' 
+            name='image_url' 
             placeholder='image url'
+            onChange={handleChanges}
+            value={formVal.img_url}
              />
         </label>
         <label>
           <input 
             type='text'
-            name='name' 
-            placeholder='name'
+            name='species' 
+            placeholder='species'
+            onChange={handleChanges}
+            value={formVal.species}
              />
         </label>
         <label>
           <input 
             type='text'
-            name='name' 
+            name='h2o_frequency' 
             placeholder='h2o frequency'
+            onChange={handleChanges}
+            value={formVal.h2o_frequency}
              />
         </label>
 
-        <button className='add-plant-button'>Submit</button>
+        <button type='submit' className='add-plant-button'>Submit</button>
       </form>
     </div>
   )
