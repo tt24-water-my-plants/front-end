56c561eff0c2a1b6c2de382afe9c0cb896229826
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-18 15:10:06 -0500

    created add form, need to add functionality now

--- src/Components/NewPlant.js
+++ src/Components/NewPlant.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import './componentStyles/NewPlant.css';
+
+
+
 
 const NewPlant = () => {
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log('click!');
+  }
+
+
   return(
-    <div></div>
+    <div className='form-container'>
+      <form className='add-plant-form' onSubmit={handleSubmit}>
+      <label>
+          <input 
+            type='text'
+            name='name' 
+            placeholder='image url'
+             />
+        </label>
+        <label>
+          <input 
+            type='text'
+            name='name' 
+            placeholder='name'
+             />
+        </label>
+        <label>
+          <input 
+            type='text'
+            name='name' 
+            placeholder='h2o frequency'
+             />
+        </label>
+
+        <button className='add-plant-button'>Submit</button>
+      </form>
+    </div>
   )
 }
 
