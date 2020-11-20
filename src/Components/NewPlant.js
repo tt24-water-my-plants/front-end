63fd1ec1fe01b687efb2325afd90ccc4fe0a753f
s-munro <68410363+s-munro@users.noreply.github.com>
2020-11-19 20:47:16 -0500

    add form, need to figure out API.  User ID for post request.

--- src/Components/NewPlant.js
+++ src/Components/NewPlant.js
@@ -1,8 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './componentStyles/NewPlant.css';
+import axiosWithAuth from '../utils/axiosWithAuth';
+import { useHistory } from 'react-router-dom'
+
+const initialPlantValues = {
+  nickname: '',
+  species: '',
+  h20_frequency: '',
+  img_url: '',
+}
 
 const NewPlant = () => {
+
+  const [plant, setPlant] = useState(initialPlantValues);
+  const { push } = useHistory();
+
+const handleChange = (e) => {
+  e.persist();
+  let value = e.target.value;
+  setPlant({
+    ...plant,
+    [e.target.name]: value
+  });
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log('heythere');
+  axiosWithAuth()
+    .post(`/users/:id/plants`) //2 is just a placeholder.  needs to be user ID??????
+    .then((res) => {
+      // props.setPlants(res.data)
+      console.log('setplants this? : ', res.data);
+      push('/Plants');
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+};
+
   return(
-    <div></div>
+      <div className='new-form-container'>
+      <form className='new-plant-form' onSubmit={handleSubmit}>
+
+      <label>
+          Nickname
+          <input 
+          type='text'
+          name='nickname'
+          value={plant.nickname}
+          placeholder='nickname, i.e. Frank'
+          onChange={handleChange}
+           />
+        </label>
+
+        <label>
+          Image URL
+          <input 
+          type='text'
+          name='species'
+          value={plant.species}
+          placeholder='species'
+          onChange={handleChange}
+           />
+        </label>
+
+        <label>
+          Species
+          <input 
+          type='text'
+          name='h20_frequency'
+          value={plant.h20_frequency}
+          placeholder='i.e. daily, weekly'
+          onChange={handleChange}
+           />
+        </label>
+
+        <label>
+          Watering Frequency
+          <input 
+          type='text'
+          name='img_url'
+          value={plant.img_url}
+          placeholder='image url'
+          onChange={handleChange}
+           />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
--- src/Components/UpdatePlant.js
+++ src/Components/UpdatePlant.js
@@ -3,7 +3,47 @@ import React from 'react';
 const UpdatePlant = () => {
 
   return(
-    <div></div>
+    <div className='update-form-container'>
+      <form>
+
+      <label>
+          Nickname
+          <input 
+          type='text'
+          name='nickname'
+          // value={}
+           />
+        </label>
+
+        <label>
+          Image URL
+          <input 
+          type='text'
+          name='img_url'
+          // value={}
+           />
+        </label>
+
+        <label>
+          Species
+          <input 
+          type='text'
+          name='species'
+          // value={}
+           />
+        </label>
+
+        <label>
+          Watering Frequency
+          <input 
+          type='text'
+          name='h2o_frequency'
+          // value={}
+           />
+        </label>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
